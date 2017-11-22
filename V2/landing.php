c6d4b83ab2e31bc1b7fbe1563db2f13d7b17cd3c
LujanSoft <fredyyadriana@gmail.com>
2017-11-22 07:29:16 -0500

    Landing de Gisell

--- V2/landing.php
+++ V2/landing.php
@@ -1,29 +1,62 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <script type="text/js" src="jquery.main.js"></script>
-  <script type="text/js"></script>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/css/materialize.min.css">
   <link rel="stylesheet" href="views/assets/css/main.css">
   <title>Landing Page WhatFood</title>
 </head>
 <body>
-    <div class="bienvenida--landing">
-        <section>
-        <div class="logo"> <img src="views/assets/image/logo-whatfood.png"> </div>
-        <div class="intro">
-            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna
-            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-            laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur
-            adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
-            ea commodo consequat.</p>
-       </div>
-          <div class="cap"><a href="dashboard" class="botonrojo">INICIAR SESION</a></div>
+  <div class="bienvenida--landing">
+    <div class="capa">
+      <header>
+          <div class="header-navigation fix-nav">
+          <nav class="barr">
+              <div class="nav-wrapper">
+                  <a href="" class="brand-logo" id="logo-landing"><p>WhatFood</p></a>
+                  <ul id="nav-mobile" class="right hide-on-med-and-down naves">
+                      <li><a href="#quees" class="what">¿Que es WhatFood?</a></li>
+                      <li><a href="dashboard">Iniciar Sesión</a></li>
+                      <li><a href="crear-auth-visitor">Registrate Gratis</a></li>
+                      <li><a href="http://www.facebook.com" class="app1"><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+                      <li><a href="http://www.gmail.com" class="app1"><i class="fa fa-google" aria-hidden="true"></i></a></li>
+                      <li><a href="http://www.twitter.com" class="app1"><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+                  </ul>
+              </div>
+          </nav>
+          </div>
+      </header>
+      <div class="logo"> <img src="views/assets/image/logo-blanco.png"> </div>
+        <section >
+        <div class="rp">
+          <p>REGISTRATE GRATIS</p>
+          <p class="wtf">WhatFood es una aplicación que te va a permitir encontrar sitios de comida rapida y
+          restaurantes que se encuentran cerca de tu ubicacion.</p>
+          <p class="WF">Simplemente registrate de forma gratuita y comienza a disfrutar los beneficios
+          que WhatFood trae para ti.</p>
+          <div class="cap"><a href="crear-auth-visitor" class="boton">Deseo Registrarme!</a></div>
+        </div>
+          <!-- <div class="cap"><a href="dashboard" class="botonrojo">INICIAR SESION</a></div> -->
       </section>
-    </div>
+  </div>
+</div>
+<div class="landing--two">
+  <div class="silueta" id="quees"><img src="views/assets/image/silueta.png"> </div>
+  <div class="que-es" ><img src="views/assets/image/que-es.png"> </div>
+  <div class="plato"><img src="views/assets/image/plat.jpg"> </div>
+  <div class="intro"  >
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit,
+      sed do eiusmod tempor incididunt ut labore et dolore magna
+      aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+      laboris nisi ut aliquip ex ea commodo consequat. Lorem ipsum dolor sit amet, consectetur
+      adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex
+      ea commodo consequat.</p>
+  </div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.100.2/js/materialize.min.js"></script>
 </body>
 </html>
